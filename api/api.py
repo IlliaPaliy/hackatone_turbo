9b4ef209d454f197b119d8ecafc4301aa9051992
Illia <illiapaliy@knu.ua>
2023-12-16 11:20:38 +0200

    updated route

--- api/api.py
+++ api/api.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/check_connection', methods=['GET'])
+@app.route('/', methods=['GET'])
 def check_connection():
     return jsonify({'status': 'success', 'message': 'Database connection is successful'})
 
