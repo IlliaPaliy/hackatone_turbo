44eca46bb8817030a9bcb5d3d9b16af50a3517f5
Illia <illiapaliy@knu.ua>
2023-12-16 16:27:33 +0200

    added name and grades generator

--- api/api.py
+++ api/api.py
@@ -1,8 +1,15 @@
 from flask import Flask, jsonify
-
+import api.data_generator
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def check_connection():
     return jsonify({'status': 'success', 'message': 'Database connection is successful'})
 
+@app.route('/get-grades')
+def get_grades():
+    return jsonify({'status': 'success', 'grades': api.data_generator.get_marks()})
+
+@app.route('/get-name')
+def get_name():
+    return jsonify({'status': 'success', 'name': api.data_generator.get_name()})
